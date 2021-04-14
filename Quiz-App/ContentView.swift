bf3ff8ee64e2ab8c02c4a3387c8c4c3af09a1d94
kid43 <hphongug0210@gmail.com>
2021-04-14 08:35:11 +0700

    Initial Commit

---
+++ Quiz-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz-App
+//
+//  Created by Phong Le on 14/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz-App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz-App
+//
+//  Created by Phong Le on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
